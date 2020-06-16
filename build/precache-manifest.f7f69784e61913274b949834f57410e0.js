13bbeb21f7936256d8941b28ef9c63e64dea5d4b
ryan <hs_18312833057@yeah.net>
2020-06-16 10:38:26 +0800

    test

---
+++ build/precache-manifest.f7f69784e61913274b949834f57410e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33076e4defe319558409f37120fb8962",
+    "url": "/react-demo/build/index.html"
+  },
+  {
+    "revision": "97f71c1db7fc8ae02e9a",
+    "url": "/react-demo/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e2a1f5fdaf1895e1d4b",
+    "url": "/react-demo/build/static/js/2.e8c57356.chunk.js"
+  },
+  {
+    "revision": "f051717053f464d6125e7926ab703fa1",
+    "url": "/react-demo/build/static/js/2.e8c57356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f71c1db7fc8ae02e9a",
+    "url": "/react-demo/build/static/js/main.5721077c.chunk.js"
+  },
+  {
+    "revision": "b361e3036e8a8a2cc1d8",
+    "url": "/react-demo/build/static/js/runtime-main.b2762266.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
